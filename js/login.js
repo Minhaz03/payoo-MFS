2e81ed1d1c745e016e29aae016bc98a9cb4fac66
Minhaz03 <minhazulalamsohan2002@gmail.com>
2024-09-19 15:57:12 +0600

    third commit

--- js/login.js
+++ js/login.js
@@ -1,11 +1,28 @@
 // console.log('dom is installed')
 
-
+// step-1- set event handler
 document.getElementById('btn-login').addEventListener('click', function(event){
+    // step-2- prevent default behavior (prevent reloading browser)
+
     event.preventDefault();  
-    console.log('login button');
 
+    // console.log('login button');
 
+    // step-3- get the phone number
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value; 
+
+    console.log(phoneNumber, pinNumber);
+
+    // step-4- validate phone and pin
+    // this is temporary not suitable to use
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('You are logged in');
+        // step-5- allow user to use the website
+        window.location.href = '/home.html'
+    }
+    else{
+        console.log('Wrong phone or pin try again');
+    }
+
 })
