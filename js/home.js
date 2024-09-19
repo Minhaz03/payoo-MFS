4bd30d93acae217c60f9ba2a864d7a400e472658
Minhaz03 <minhazulalamsohan2002@gmail.com>
2024-09-19 16:31:31 +0600

    add money & pin input completed

--- js/home.js
+++ js/home.js
@@ -12,4 +12,26 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     // get the pin number
     const pinNumberInput =  document.getElementById('input-pin-number').value;
     console.log(pinNumberInput);
+
+    // step-3- verify the pin number && not the right way
+    if (pinNumberInput === '1234'){
+        console.log('adding money to account')
+
+        // step-4- get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(balance)
+
+        // step-5- add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // step-6- update the balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+
+    }
+    else{
+        console.log('failed pls try again')
+    }
 });
